56624c48a97127bb302b19e6219d7c51667fa8ff
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-07-05 19:54:53 +0800

    minor fix

--- app/assets/javascripts/coreui/utils.js
+++ app/assets/javascripts/coreui/utils.js
@@ -134,10 +134,6 @@ function initSidebar() {
       $(this).parents('.c-sidebar-nav-dropdown').addClass('c-show');
     }
   });
-
-  if ($.cookie('sidebar') === '1') {
-    $('#sidebar').addClass('c-sidebar-unfoldable');
-  }
 }
 
 $(document).ready(function () {
